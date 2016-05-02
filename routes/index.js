8c88cf97f283325304964ed1a5f27c6b802a4449
yxp <624835942@qq.com>
2016-05-02 23:05:18 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.render('index', {title: '主页'});
+    });
+    //注册
+    app.get('/reg', function (req, res) {
+        res.render('reg', {title: '注册'});
+    });
+    app.post('/reg', function (req, res) {
+    });
+
+    //登录
+    app.get('/login', function (req, res) {
+        res.render('login', {title: '登录'});
+    });
+    app.post('/login', function (req, res) {
+    });
+    //发表文章
+    app.get('/post', function (req, res) {
+        res.render('post', {title: '发表'});
+    });
+    app.post('/post', function (req, res) {
+    });
+    //登出
+    app.get('/logout', function (req, res) {
+    });
+};

fb798034a79b8989aaaa77673ff27e1e3f459956
yxp <624835942@qq.com>
2016-05-15 21:02:08 +0800

    添加文章检索功能

--- routes/index.js
+++ routes/index.js
@@ -223,6 +223,23 @@ module.exports = function (app) {
         });
     });
 
+
+    app.get('/search', function (req, res) {
+        Post.search(req.query.keyword, function (err, posts) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('search', {
+                title: "SEARCH:" + req.query.keyword,
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
+
     //按用户名获取文章
     app.get('/u/:name', function (req, res) {
         var page = parseInt(req.query.p) || 1;
